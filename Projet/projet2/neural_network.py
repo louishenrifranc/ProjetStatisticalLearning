5a550eeb6f480e0ea38c82642917babb9233d46b
Louis Henri Franc <louis.franc@insa-rouen.fr>
2016-12-07 13:50:16 -0500

    add patience

--- Projet/projet2/neural_network.py
+++ Projet/projet2/neural_network.py
@@ -13,6 +13,10 @@ from sklearn.preprocessing import normalize
 
 
 def read_file():
+    """
+    Read the file and remove confusing informations
+    :return:
+    """
     data = pd.read_csv('kc_house_data.csv')
     data = data.drop(['id', 'date', 'sqft_lot', 'sqft_above', 'sqft_basement', 'yr_renovated', 'zipcode', 'lat', 'long',
                       'sqft_living15', 'sqft_lot15'], 1)
@@ -23,7 +27,7 @@ def read_file():
     return data
 
 
-plt.axis([0, 100, 150000, 300000])
+plt.axis([0, 1000, 150000, 300000])
 plt.ion()
 
 
@@ -42,16 +46,20 @@ def plot(train_loss, test_loss):
 
 
 class NN():
-    def __init__(self, data):
+    def __init__(self,
+                 data,
+                 batch_size):
+
         self.data = data
         self.N_EXAMPLES = len(data)
-        self.BATCH_SIZE = 32
+        self.BATCH_SIZE = batch_size
         self.N_INPUT = 9
         self.N_OUTPUT = 1
 
         X = T.fmatrix('X')
         Y = T.fvector('Y')
         dropout_prob = T.scalar('p_drop', dtype=theano.config.floatX)
+        rmsprop_ro = T.scalar('ro_rms', dtype=theano.config.floatX)
 
         # About batch_norm : This layer should be inserted between a linear transformation
         # (such as a DenseLayer, or Conv2DLayer) and its nonlinearity.
@@ -80,7 +88,7 @@ class NN():
         self.model['l_hid3'] = DenseLayer(self.model['l_hid2'],
                                           self.N_INPUT * 2)
         self.model['l_hid3'] = lasagne.layers.batch_norm(self.model['l_hid3'])
-        self.model['l_hid3'] = DropoutLayer(self.model['l_hid3'], p=dropout_prob)
+        # self.model['l_hid3'] = DropoutLayer(self.model['l_hid3'], p=dropout_prob)
 
         # Hidden 4
         self.model['l_hid4'] = DenseLayer(self.model['l_hid3'],
@@ -96,7 +104,7 @@ class NN():
                                                                   penalty=lasagne.regularization.l2)
         loss += l2_reg
         all_params = lasagne.layers.get_all_params(self.model['l_out'], trainable=True)
-        updates = lasagne.updates.adadelta(loss, all_params)
+        updates = lasagne.updates.rmsprop(loss, all_params, rho=rmsprop_ro)
 
         # Loss expression for testing
         out_test = lasagne.layers.get_output(self.model['l_out'], deterministic=True)
@@ -104,14 +112,14 @@ class NN():
         loss_test = T.mean(T.sqr(T.sub(out_test, Y)), axis=0)
         rmse_test = T.sqrt(loss_test)
 
-        self.train_fn = theano.function([X, Y, dropout_prob], [loss], updates=updates)
+        self.train_fn = theano.function([X, Y, dropout_prob, rmsprop_ro], [loss], updates=updates)
         self.test_fn = theano.function([X, Y], rmse_test)
 
         self.create_batches()
 
     def create_batches(self):
-        self.p_train_examples = 0.7
-        train_ind = random.sample(xrange(len(self.data)), k=(int)(0.7 * len(self.data)))
+        self.p_train_examples = 0.8
+        train_ind = random.sample(xrange(len(self.data)), k=(int)(self.p_train_examples * len(self.data)))
         X_train = self.data.ix[train_ind, self.data.columns != 'price'].as_matrix().astype(theano.config.floatX)
 
         Y_train = self.data.ix[train_ind, self.data.columns == 'price'].as_matrix().astype(theano.config.floatX)
@@ -135,7 +143,7 @@ class NN():
             excerpt = indices[start_idx:start_idx + self.BATCH_SIZE]
             yield self.data[train_or_test][0][excerpt], self.data[train_or_test][1][excerpt]
 
-    def save_model(self, output_layer, filename='model.p'):
+    def save_model(self, output_layer, filename='model1.p'):
         """Pickels the parameters within a Lasagne model."""
         data = lasagne.layers.get_all_param_values(output_layer)
         filename = os.path.join('./', filename)
@@ -154,7 +162,15 @@ class NN():
         self.NUM_EPOCH = 1000
         train_loss = []
         test_loss = []
-        p_drop = 0.1
+        p_drop = 0.2
+        ro_rms = 0.9
+
+        time_patience = 10
+        is_improving = False
+        max_time_patience = 30
+        max_time_improving = 5
+        max_improvement_patience = 0.3
+
         for epoch in range(self.NUM_EPOCH):
             train_err = 0
             n_train_batches = 0
@@ -163,7 +179,7 @@ class NN():
                 X_batch_train, y_batch_train = batch_train
                 #  print(X_batch_train.shape, y_batch_train.shape)
 
-                err_train = self.train_fn(X_batch_train, y_batch_train, p_drop)
+                err_train = self.train_fn(X_batch_train, y_batch_train, p_drop, ro_rms)
                 # if random.random() < 0.05:
                 #    print(err_train[1], err_train[2], err_train[3])
                 train_err += err_train[0]
@@ -177,20 +193,52 @@ class NN():
                 err = self.test_fn(X_batch_val, y_batch_val)
                 val_rmse += err
                 n_val_batches += 1
-            train_loss.append(train_err / n_train_batches)
+
+            # Patience and then improve
+            new_train_error = np.sqrt(train_err / n_train_batches)
+            if len(train_loss) > 5:
+                if min(train_loss) < new_train_error and not is_improving:
+                    time_patience -= 1
+                    #print('Decrease patience %d' % time_patience)
+                    if time_patience == 0:
+                        time_patience = max_time_improving
+                        is_improving = True
+                        ro_rms -= max_improvement_patience
+                        #print('[Update] Increasing ro %f' % ro_rms)
+                elif min(train_loss) > new_train_error and not is_improving and time_patience != 10:
+                    time_patience = 10
+                    #print('Found new minimum, reset patience')
+                if is_improving:
+                    time_patience -= 1
+                    ro_rms += (max_improvement_patience / (max_time_improving))
+                    if time_patience == 0:
+                        time_patience = max_time_patience
+                        is_improving = False
+                        #print('End patience %f' % ro_rms)
+                    #print('Still ro_rms = %f' % ro_rms)
+
+            train_loss.append(new_train_error)
             test_loss.append(val_rmse / n_val_batches)
-            plot(np.sqrt(train_loss), test_loss)
+            # plot(train_loss, test_loss)
             print("Epoch {} of {} took {:.3f}s".format(
                 epoch + 1, self.NUM_EPOCH, time.time() - start_time))
-            print("  training loss:\t\t{:.6f}".format(train_err / n_train_batches))
-            print("  validation loss:\t\t{:.6f}".format(val_rmse / n_val_batches))
+            print("  training loss:\t\t{:.6f}".format(new_train_error))
+            print("  validation loss:\t\t{:.6f}\n".format(val_rmse / n_val_batches))
+
             if test_loss[-1] == min(test_loss):
                 self.save_model(self.model['l_out'])
                 print('\t\tModel saved')
-            p_drop += (0.4 / self.NUM_EPOCH)
+
+                # Increasing dropout over time
+                # Zp_drop += (0.4 / self.NUM_EPOCH)
+
+        cPickle.dump(train_loss, open('train_loss1', 'wb'))
+        cPickle.dump(test_loss, open('test_loss1', 'wb'))
+        plt.savefig('foo.png')
+
 
 if __name__ == '__main__':
     # read_file()
     data = pd.read_pickle('data.p')
-    neural_network = NN(data)
+    neural_network = NN(data, batch_size=32)
     neural_network.train()
