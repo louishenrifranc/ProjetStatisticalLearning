a43f78ac633c5de7684e0f5f4d0f3a1160365865
Louis Henri Franc <louis.franc@insa-rouen.fr>
2016-12-06 22:57:53 -0500

    neural network work

--- Projet/projet2/neural_network.py
+++ Projet/projet2/neural_network.py
@@ -6,6 +6,7 @@ import theano
 import random
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 
 def read_file():
@@ -18,49 +19,84 @@ def read_file():
     return data
 
 
+plt.axis([0, 100, 150000, 300000])
+plt.ion()
+
+
+def plot(train_loss, test_loss):
+    """
+    Plot a training loss and testing loss in realtime
+    :param train_loss: vector of all previous value taken for the training loss
+    :param test_loss: vector of all previous value taken for the testing loss
+    :return: .
+    """
+    plt.scatter(len(train_loss), train_loss[-1], marker='o', c='r', s=0.01)
+    plt.scatter(len(test_loss), test_loss[-1], marker='v', c='b', s=0.01)
+    plt.plot(np.arange(len(train_loss)), train_loss, c='r', linewidth=1.2, label='train loss')
+    plt.plot(np.arange(len(test_loss)), test_loss, c='b', linewidth=1.2, label='test loss')
+    plt.pause(0.05)
+
+
 class NN():
     def __init__(self, data):
         self.data = data
         self.N_EXAMPLES = len(data)
-        self.BATCH_SIZE = 32
+        self.BATCH_SIZE = 16
         self.N_INPUT = 9
         self.N_OUTPUT = 1
 
         X = T.fmatrix('X')
         Y = T.fvector('Y')
+
         model = {}
         model['l_in'] = InputLayer(input_var=X, shape=(self.BATCH_SIZE, self.N_INPUT))
         model['l_hid1'] = DenseLayer(model['l_in'],
+                                     self.N_INPUT * 10)
+        model['l_hid1'] = DropoutLayer(model['l_hid1'], p=0.2)
+        model['l_hid2'] = DenseLayer(model['l_hid1'],
+                                     self.N_INPUT * 5)
+        model['l_hid2'] = DropoutLayer(model['l_hid2'], p=0.2)
+        model['l_hid3'] = DenseLayer(model['l_hid2'],
                                      self.N_INPUT * 2)
-        model['l_drop1'] = DropoutLayer(model['l_hid1'], 0.3)
-        model['l_hid2'] = DenseLayer(model['l_drop1'],
+        model['l_hid3'] = DropoutLayer(model['l_hid3'], p=0.2)
+        model['l_hid4'] = DenseLayer(model['l_hid3'],
                                      self.N_INPUT)
-        model['l_out'] = DenseLayer(model['l_hid2'], 1)
+        # model['l_drop1'] = DropoutLayer(model['l_hid1'], 0.3)
+        model['l_out'] = DenseLayer(model['l_hid4'], 1)
 
+        # Loss expression for training
         out = lasagne.layers.get_output(model['l_out'])
+        out = T.reshape(out, (out.shape[0],))
+        loss = T.mean(T.sqr(T.sub(out, Y)), axis=0)
+        l2_reg = lasagne.regularization.regularize_network_params({model['l_out']: 0.01},
+                                                                  penalty=lasagne.regularization.l2)
+        loss += l2_reg
+        all_params = lasagne.layers.get_all_params(model['l_out'], trainable=True)
+        updates = lasagne.updates.adadelta(loss, all_params)
 
-        loss = T.square(out - Y)
-        cost = T.mean(loss)
-        rmse = T.square(cost)
+        # Loss expression for testing
+        out_test = lasagne.layers.get_output(model['l_out'], deterministic=True)
+        out_test = T.reshape(out_test, (out_test.shape[0],))
+        loss_test = T.mean(T.sqr(T.sub(out_test, Y)), axis=0)
+        rmse_test = T.sqrt(loss_test)
 
-        all_params = lasagne.layers.get_all_params(model['l_out'])
-        objectives = lasagne.updates.adadelta(cost, all_params)
-
-        self.train_fn = theano.function([X, Y], cost, updates=objectives)
-        self.test_fn = theano.function([X, Y], rmse)
+        self.train_fn = theano.function([X, Y], [loss], updates=updates)
+        self.test_fn = theano.function([X, Y], rmse_test)
 
         self.create_batches()
 
     def create_batches(self):
         self.p_train_examples = 0.7
-        train_ind = random.sample(len(self.data), k=(int)(0.7 * len(self.data)))
+        train_ind = random.sample(xrange(len(self.data)), k=(int)(0.7 * len(self.data)))
+        X_train = self.data.ix[train_ind, self.data.columns != 'price'].as_matrix().astype(theano.config.floatX)
 
-        X_train = self.data[train_ind, self.data.columns != 'price'].as_matrix().astype(theano.config.floatX)
-        Y_train = self.data[train_ind, 'price'].as_matrix().astype(theano.config.floatX)
+        Y_train = self.data.ix[train_ind, self.data.columns == 'price'].as_matrix().astype(theano.config.floatX)
+        Y_train = np.reshape(Y_train, (Y_train.shape[0],))
 
         self.data = self.data.drop(self.data.index[train_ind])
-        X_test = self.data[:, self.data.columns != 'price'].as_matrix().astype(theano.config.floatX)
-        Y_test = self.data[:, 'price'].as_matrix().astype(theano.config.floatX)
+        X_test = self.data.ix[:, self.data.columns != 'price'].as_matrix().astype(theano.config.floatX)
+        Y_test = self.data.ix[:, self.data.columns == 'price'].as_matrix().astype(theano.config.floatX)
+        Y_test = np.reshape(Y_test, (Y_test.shape[0],))
 
         self.data = {
             'train': [X_train, Y_train],
@@ -68,35 +104,49 @@ class NN():
         }
 
     def get_next_batches(self, train_or_test):
-        indices = np.arange(len(self.N_EXAMPLES))
-        for start_idx in range(0, self.N_EXAMPLES - self.BATCH_SIZE + 1, self.BATCH_SIZE):
+        nb_examples = np.shape(self.data[train_or_test][0])[0]
+        indices = np.arange(nb_examples)
+        np.random.shuffle(indices)
+        for start_idx in range(0, nb_examples - self.BATCH_SIZE + 1, self.BATCH_SIZE):
             excerpt = indices[start_idx:start_idx + self.BATCH_SIZE]
-            yield self.data[train_or_test][0][excerpt, :], self.data[train_or_test][1][excerpt]
+            yield self.data[train_or_test][0][excerpt], self.data[train_or_test][1][excerpt]
 
-    def train(self, train_or_test):
-        for epoch in range(100):
+    def train(self):
+        self.NUM_EPOCH = 100
+        train_loss = []
+        test_loss = []
+        for epoch in range(self.NUM_EPOCH):
             train_err = 0
             n_train_batches = 0
             start_time = time.time()
-            for X_batch_train in self.get_next_batches('train'):
-                err_train = self.train_fn(theano.shared(X_batch_train))
-                train_err += err_train
+            for batch_train in self.get_next_batches('train'):
+                X_batch_train, y_batch_train = batch_train
+                #  print(X_batch_train.shape, y_batch_train.shape)
+
+                err_train = self.train_fn(X_batch_train, y_batch_train)
+                # if random.random() < 0.05:
+                #    print(err_train[1], err_train[2], err_train[3])
+                train_err += err_train[0]
                 n_train_batches += 1
 
-            val_err = 0
+            val_rmse = 0
             val_rec_err = 0
             n_val_batches = 0
-            for X_batch_val in self.get_next_batches('train'):
-                err = self.test_fn(theano.shared(X_batch_val))
-                val_err += err[0]
-                val_rec_err += err[1]
+            for batch_val in self.get_next_batches('train'):
+                X_batch_val, y_batch_val = batch_val
+                err = self.test_fn(X_batch_val, y_batch_val)
+                val_rmse += err
                 n_val_batches += 1
+            train_loss.append(train_err / n_train_batches)
+            test_loss.append(val_rmse / n_val_batches)
+            plot(np.sqrt(train_loss), test_loss)
             print("Epoch {} of {} took {:.3f}s".format(
                 epoch + 1, self.NUM_EPOCH, time.time() - start_time))
             print("  training loss:\t\t{:.6f}".format(train_err / n_train_batches))
-            print("  validation loss:\t\t{:.6f}".format(val_err / n_val_batches))
+            print("  validation loss:\t\t{:.6f}".format(val_rmse / n_val_batches))
 
 
 if __name__ == '__main__':
     data = read_file()
     neural_network = NN(data)
+    neural_network.train()
